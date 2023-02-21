e912c2a7b755c571a0b963e72bda91bed469045e
Natsuki Yagi <straitwalk@gmail.com>
2023-02-21 22:21:35 +0900

    add validator tests

--- tests/test.rs
+++ tests/test.rs
@@ -1,3 +1,4 @@
+use assert_matches::assert_matches;
 use evil::Omit;
 
 #[derive(Omit, Debug)]
@@ -22,6 +23,25 @@ fn test_new_hoge() {
     assert_eq!(hoge.clone().age, 0);
 }
 
+#[test]
+fn test_validator() {
+    use validator::Validate;
+
+    #[derive(Omit, Debug, Validate)]
+    #[omit(NewData, id, derive(Debug, Validate))]
+    struct Data {
+        #[validate(range(min = 1))]
+        pub id: u64,
+        #[validate(range(min = 18, max = 20))]
+        pub age: u32,
+    }
+
+    let data = Data { id: 1, age: 1 };
+    assert_matches!(data.validate(), Err(_));
+
+    let data = NewData { age: 1 };
+    assert_matches!(data.validate(), Err(_));
+}
 // #[test]
 // #[should_panic]
 // fn test_enum() {
